97b7bed1d48ebdf7657ff23859eab6e4f9c93c44
osip123 <131646261+osip123@users.noreply.github.com>
2024-02-25 21:12:11 +0300

    sidebar add

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,13 @@
-import Header from './header/Header'
 import Aside from './aside/Aside'
 import { Outlet } from 'react-router-dom'
 import './Layout.scss'
 
 const Layout = () => {
   return (
-    <>
-    <Header />
     <main>
-        <Aside />
-        <Outlet />
+      <Aside />
+      <Outlet />
     </main>
-    </>
   )
 }
 
